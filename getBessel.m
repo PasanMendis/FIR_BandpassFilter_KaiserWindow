452998d78a8ed26bba7f52aaec7a0d46e8e799c4
PasanMendis <58726550+PasanMendis@users.noreply.github.com>
2020-10-02 17:57:12 +0530

    Update getBessel.m

--- getBessel.m
+++ getBessel.m
@@ -1,6 +1,6 @@
 %Bessel function is a mathematical function needed to generate the Kaiser window in FIR filter
 function [ Ix ] = getBessel(x)% k is considered as 500
-sum=1;
+sum=1; #Intialize sum
 for k=1:500
     sum=sum+((1/factorial(k))*(x/2).^k).^2;
 end
